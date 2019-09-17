34e3e6627d1cbd1eb905064c6121d43df3bdda82
juliano-cunha <julianocunhatrabalho@gmail.com>
2019-09-17 16:29:22 -0300

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\juliano.cunha\PycharmProjects\controle_gastos\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
