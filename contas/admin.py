82ffdb3572d26e996cb70553f54454022f8e0876
Juliano Cunha <julianocunhatrabalho@gmail.com>
2019-09-17 23:05:07 -0300

    17/09 - Classes Categorias
    Criadas classe de categorias, atualizado models

--- contas/admin.py
+++ contas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# importa do nosso diretorio as classes criadas
+from .models import Categoria
+from .models import Transacao
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Transacao)
--- contas/models.py
+++ contas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    """Classe para as categorias de gastos herdando da classe de models django"""
+    nome = models.CharField(max_length=100)
+    dt_criacao = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        """Funcao que corrige o nome mostrado no admin da categoria, não apenas o object"""
+        return self.nome
+
+
+class Transacao(models.Model):
+    """Classe para as transações do programa"""
+    data = models.DateTimeField()
+    descricao = models.CharField(max_length=200)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+    # relacionamento do campo com o banco e outra classe
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    observacoes = models.TextField(null=True, blank=True)
+
+    class Meta:
+        """utiliza a meta classe para corrigir o nome no plural, django docs """
+        verbose_name_plural = 'Transações'
+
+    def __str__(self):
+        """Funcao que corrige o nome mostrado no admin"""
+        return self.descricao
