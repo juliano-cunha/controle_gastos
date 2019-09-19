38872478581b9f80ff45490247d775d62bcb9d4e
juliano-cunha <julianocunhatrabalho@gmail.com>
2019-09-19 11:29:02 -0300

    Alterado template
    Template home, alterado.

--- contas/views.py
+++ contas/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
+from .models import Transacao
 # Create your views here.
 from django.http import HttpResponse
 import datetime
 
 
 def home(request):
-    #now = datetime.datetime.now()
-    #html = "<html><body>It is now %s.</body></html>" % now
+    data = {}
+    data['transacoes'] = ['t1', 't2', 't3']
+    data['now'] = datetime.datetime.now()
+    # html = "<html><body>It is now %s.</body></html>" % now
+    # return HttpResponse(html)
 
-    #return HttpResponse(html)
-
-    return render(request, 'contas/home.html')
+    return render(request, 'contas/home.html', data)
--- controle_gastos/urls.py
+++ controle_gastos/urls.py
@@ -19,5 +19,5 @@ from contas.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', home),
+    path('', home),
 ]
