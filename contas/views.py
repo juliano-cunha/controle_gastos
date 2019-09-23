b6f19420757ed82b6892398e2fb0a7a9364bd29c
juliano-cunha <julianocunhatrabalho@gmail.com>
2019-09-23 13:52:03 -0300

    CRUD
    Implementação do Crud da aplicação

--- contas/views.py
+++ contas/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Transacao
+from .forms import TransacaoForm
 # Create your views here.
 from django.http import HttpResponse
 import datetime
@@ -13,3 +14,42 @@ def home(request):
     # return HttpResponse(html)
 
     return render(request, 'contas/home.html', data)
+
+
+def listagem(request):
+    """imprime a listaem na tela"""
+    data={}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
+
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
+
+
+def update(request, pk):
+    data = {}
+    transacao = Transacao.objects.get(pk=pk)
+    form = TransacaoForm(request.POST or None, instance=transacao)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    data[transacao] = transacao
+    return render(request, 'contas/form.html', data)
+
+
+def delete(request, pk):
+    transacao = Transacao.objects.get(pk=pk)
+    transacao.delete()
+    return redirect('url_listagem')
